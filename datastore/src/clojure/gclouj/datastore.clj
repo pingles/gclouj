9c43d2a014e36100b8e0e25984baa7e134e64daf
Paul Ingles <paul@oobaloo.co.uk>
2016-02-01 11:11:15 +0000

    add entity persistence and retrieval- most attribute values are converted

--- datastore/src/clojure/gclouj/datastore.clj
+++ datastore/src/clojure/gclouj/datastore.clj
@@ -1,7 +1,10 @@
 (ns gclouj.datastore
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.walk :as w])
   (:import [gclouj DatastoreOptionsFactory]
-           [com.google.gcloud.datastore DatastoreOptions Entity FullEntity DatastoreOptions$DefaultDatastoreFactory Transaction TransactionOption Key IncompleteKey DatastoreBatchWriter]
+           [java.io InputStream]
+           [java.nio ByteBuffer]
+           [com.google.gcloud.datastore DatastoreOptions Entity FullEntity DatastoreOptions$DefaultDatastoreFactory Transaction TransactionOption Key IncompleteKey DatastoreBatchWriter EntityValue ValueType StringValue LongValue DoubleValue DateTime DateTimeValue BooleanValue BlobValue Blob NullValue Value KeyValue FullEntity$Builder]
            [com.google.gcloud AuthCredentials]))
 
 (defn credential-options [project-id namespace json-key]
@@ -21,21 +24,58 @@
 (defn incomplete-key [project-id kind]
   (.build (IncompleteKey/builder project-id kind)))
 
-
-(defn get-entity [^Transaction txn key]
-  (.get txn key))
+(defn get-entity
+  ([^Transaction txn key]
+   (.get txn key))
+  ([^Transaction txn key1 key2]
+   (iterator-seq (.get txn (into-array Key [key1 key2]))))
+  ([^Transaction txn key1 key2 & keys]
+   (iterator-seq (.get txn (into-array Key (concat [key1 key2] keys))))))
 
 (defn add-entity [^Transaction txn entity]
   (.add txn ^FullEntity entity))
 
-(defn entity [key m]
-  (let [b (Entity/builder key)]
-    (doseq [[k v] m]
-      (cond (associative? v) (let [attr-key (incomplete-key (.projectId key)
-                                                            (format "%s.%s" (.kind key) k))]
-                               (.set b k (entity attr-key v)))
-            :else            (.set b k v)))
-    (.build b)))
+(defn byte-array? [x]
+  (= (Class/forName "[B") (class x)))
 
-(defn transaction [service]
+(defn attrvalue [x]
+  (cond (nil? x)                      (NullValue. )
+        (float? x)                    (DoubleValue/of x)
+        (number? x)                   (LongValue/of x)
+        (string? x)                   (StringValue/of x)
+        (instance? DateTime x)        (DateTimeValue/of x)
+        (instance? Boolean x)         (BooleanValue/of x)
+        (instance? Key x)             (KeyValue/of x)
+        (or (byte-array? x)
+            (instance? InputStream x)
+            (instance? ByteBuffer x)) (BlobValue/of (Blob/copyFrom x))
+        :else      x))
+
+(defn entity
+  "Converts a Clojure map into a Datastore Entity. Recursively converts
+  all values into an entity or value using attrvalue."
+  [key m]
+  (let [builder    (Entity/builder key)]
+    (doseq [[k v] m]
+      (cond (map? v) (let [attrkey (incomplete-key (.projectId key) (format "%s.%s" (.kind key) k))]
+                       (.set builder k (entity attrkey v)))
+            :else    (.set builder k (attrvalue v))))
+    (.build builder)))
+
+
+(defprotocol ToClojure
+  (to-clojure [x]))
+(extend-protocol ToClojure
+  LongValue
+  (to-clojure [value] (.get value))
+  StringValue
+  (to-clojure [value] (.get value))
+  EntityValue
+  (to-clojure [value] (to-clojure (.get value)))
+  FullEntity
+  (to-clojure [entity]
+    (let [names (-> entity (.names) (.iterator) (iterator-seq))]
+      (into {} (map (fn [attr] [attr (to-clojure (.getValue entity attr))]) names)))))
+
+(defn transaction [service & options]
   (.newTransaction service (into-array TransactionOption [])))
--- datastore/test/datastore_test.clj
+++ datastore/test/datastore_test.clj
@@ -1,33 +1,52 @@
 (ns datastore-test
-  (:import [com.google.gcloud.datastore.testing LocalGcdHelper])
+  (:import [com.google.gcloud.datastore.testing LocalGcdHelper]
+           [com.google.gcloud.datastore NullValue Entity])
   (:require [gclouj.datastore :refer :all]
             [clojure.test :refer :all]))
 
 (def project-id "gclouj-datastore")
 
 (deftest entity-mapping
-  (let [e (entity (incomplete-key project-id "Foo")
-                  {"FirstName" "Paul"})]
-    (is (= "Paul" (.getString e "FirstName")))))
+  (let [testbytes (byte-array [1 2 3 4 5])
+        e (entity (incomplete-key project-id "Foo")
+                  {"FirstName"    "Paul"
+                   "Age"          35
+                   "Clojurist"    true
+                   "NilValue"     nil
+                   "TestBytes"    testbytes
+                   "Intelligence" 50.5
+                   "Address"      {"City" "London"}})]
+    (is (= "Paul"            (.getString e "FirstName")))
+    (is (= 35                (.getLong e "Age")))
+    (is (= 50.5              (.getDouble e "Intelligence")))
+    (is (true?               (.getBoolean e "Clojurist")))
+    (is (instance? NullValue (.getValue e "NilValue")))
+    (is (= (seq testbytes)   (seq (.toByteArray (.getBlob e "TestBytes")))))
+    (is (= "London"          (-> e (.getEntity "Address") (.getString "City"))))))
 
-(deftest nested-entity-mapping
-  (let [e (entity (incomplete-key project-id "Foo")
-                  {"Address" {"City" "London"}})]
-    (is (= "London" (-> e (.getEntity "Address") (.getString "City"))))))
-
-(deftest put-and-retrieve-entity
-  (let [port (LocalGcdHelper/findAvailablePort 9900)
-        helper (LocalGcdHelper/start project-id port)]
-    (let [opts (test-options project-id port)
-          s    (service opts)
-          t    (transaction s)]
-      (let [added         (add-entity t (entity (incomplete-key project-id "Foo")
-                                                {"FirstName" "Paul"}))
-            completed-key (.key added)]
-        (.commit t)
-        (let [t       (transaction s)
-              fetched (get-entity t completed-key)]
-          (is (= "Paul" (.getString fetched "FirstName")))
-          (.rollback t))))
-
-    (.stop helper)))
+  (deftest put-and-retrieve-entity
+    (let [port (LocalGcdHelper/findAvailablePort 9900)
+          helper (LocalGcdHelper/start project-id port)]
+      (let [opts (test-options project-id port)
+            s    (service opts)
+            t    (transaction s)]
+        (let [added         (add-entity t (entity (incomplete-key project-id "Foo")
+                                                  {"FirstName" "Paul"}))
+              added-key     (.key added)
+              added2        (add-entity t (entity (incomplete-key project-id "Foo")
+                                                  {"FirstName" "Peter"}))
+              added-key2    (.key added2)
+              added3        (add-entity t (entity (incomplete-key project-id "Foo")
+                                                  {"FirstName" "Arthur"}))
+              added-key3    (.key added3)]
+          (.commit t)
+          (let [t       (transaction s)
+                fetched (get-entity t added-key)]
+            (is (= "Paul" (.getString fetched "FirstName")))
+            (let [new-entity (Entity/builder fetched)]
+              (.set new-entity "FirstName" "Arthur"))
+            (is (= 3 (count (get-entity t added-key added-key2 added-key3))))
+            (is (= "Arthur" (-> (get-entity t added-key added-key2 added-key3)
+                                (last)
+                                (.getString "FirstName")))))))
+      (.stop helper)))
