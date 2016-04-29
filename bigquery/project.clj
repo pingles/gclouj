eda4c6d204618dc78343799c0cbc430ddea50d32
Paul Ingles <paul@oobaloo.co.uk>
2016-04-29 16:21:14 +0100

    release bigquery 0.2.0

--- bigquery/project.clj
+++ bigquery/project.clj
@@ -1,4 +1,4 @@
-(defproject gclouj/bigquery "0.2.0-SNAPSHOT"
+(defproject gclouj/bigquery "0.2.0"
   :description "Google Cloud BigQuery"
   :url "https://github.com/pingles/gclouj"
   :license {:name "Eclipse Public License"
